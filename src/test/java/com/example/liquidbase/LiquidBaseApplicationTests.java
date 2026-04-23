5bbeafd3d80469d50e1aa1d8f8cbf9e4ee0404f7
tuanha122004 <tuanhahe180076@fpt.edu.vn>
2026-04-23 10:58:42 +0700

    change to H2

--- src/test/java/com/example/liquidbase/LiquidBaseApplicationTests.java
+++ src/test/java/com/example/liquidbase/LiquidBaseApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.liquidbase;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @SpringBootTest
 class LiquidBaseApplicationTests {
