a3667168f970653461ba5328ac27ceac2e88535d
tuanha122004 <tuanhahe180076@fpt.edu.vn>
2026-04-24 10:38:36 +0700

    update

--- src/main/java/com/example/liquidbase/LiquidBaseApplication.java
+++ src/main/java/com/example/liquidbase/LiquidBaseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 //@EnableKafka
 @SpringBootApplication
 public class LiquidBaseApplication {
-    public static void main(String[] args) {SpringApplication.run(LiquidBaseApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(LiquidBaseApplication.class, args);
     }
 }
